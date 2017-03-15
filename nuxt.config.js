4f53d3ad4029984c98dcbebd742a02272c2afc1d
Devan Beitel <devan.beitel@gmail.com>
2017-03-15 12:37:45 -0500

    Moved to express based server.

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,25 +18,9 @@ module.exports = {
   */
   css: ['~assets/css/main.css'],
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
+  ** Add axios globally
   */
   build: {
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, { isClient }) {
-      if (isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+    vendor: ['axios']
   }
 }
